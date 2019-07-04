07f585a18e1132b410093b8dad5e4ab1ef876afd
python-code-level <python-training@outlook.com>
2019-07-04 13:20:36 +0100

    Create testPrint.py

---
+++ testPrint.py
@@ -0,0 +1,14 @@
+import numpy
+
+# import iris
+
+# a very long comment >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+def testPrint:
+  var = "this text"
+  return var
+
+print(testPrint)
+
+print(var)
+
